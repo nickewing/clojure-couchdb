cadcb98679c2212f841ab8bb6dcb0f4076501ff6
Alex Osborne <ato@meshy.org>
2009-11-21 18:58:13 +0800

    Build with Leiningen.

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject org.clojars.ato/clojure-couchdb "0.1.0-SNAPSHOT"
+  :description "Simple Clojure interface to Apache CouchDB."
+  :dependencies [[org.clojure/clojure "1.1.0-alpha-SNAPSHOT"]
+                 [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
+                 [org.clojure/clojure-http-client "1.0.0-SNAPSHOT"]])
