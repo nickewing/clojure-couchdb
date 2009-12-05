09195cc25a8dedce96a2cec94c161d9c3758abc1
Moritz Ulrich <ulrich.moritz@googlemail.com>
2009-12-05 19:56:15 +0100

    Increased the maintenance-version

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
-(defproject org.clojars.the-kenny/clojure-couchdb "0.1.0-SNAPSHOT"
+(defproject org.clojars.the-kenny/clojure-couchdb "0.1.1-SNAPSHOT"
   :description "Simple Clojure interface to Apache CouchDB, fork of the original project with function arguments instead of *server* and some other changes."
   :dependencies [[org.clojure/clojure "1.1.0-alpha-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
-                 [org.clojure/clojure-http-client "1.0.0-SNAPSHOT"]])
+                 [org.clojure/clojure-http-client "1.0.0-SNAPSHOT"]]
+  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]])
