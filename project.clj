2f2f0c0cbc781e46ec38580ced4d09377405a861
Moritz Ulrich <ulrich.moritz@googlemail.com>
2009-12-21 18:01:23 +0100

    Bumped version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.the-kenny/clojure-couchdb "0.1.2"
+(defproject org.clojars.the-kenny/clojure-couchdb "0.1.3"
   :description "Simple Clojure interface to Apache CouchDB, fork of the original project with function arguments instead of *server* and some other changes."
   :dependencies [[org.clojure/clojure "1.1.0-master-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
